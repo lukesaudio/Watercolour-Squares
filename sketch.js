344929a3c0bd0d09776e57ddf2b9dd0d375f5f08
Luke Child <40809085+lukesaudio@users.noreply.github.com>
2022-02-24 22:30:28 +0000

    added watercolour

--- sketch.js
+++ sketch.js
@@ -10,24 +10,35 @@ let exceededHeight = false;
 
 let backgroundColourPicker;
 
-let sliderR;
-let sliderG;
-let sliderB;
-let sliderA;
 
-let sliderRText;
-let sliderGText;
-let sliderBText;
-let sliderAText;
+
+//GUI
+var information;
+var sqaureSize;
+var squareSizeRandom;
+
+var shapeColour;
+var shapeColourAlpha;
+
+var strokeColour;
+var strokeColourAlpha;
+
+var backgroundColour;
+var backgroundColourAlpha;
+
+
 
 let pauseButton;
 let userPaused;
 
-let resetButton;
 
+
+let resetButton;
 
 let colourChanged;
 let canSetBackground;
+
+let showGUI;
 
 //timer   
 let ra=0, rb=0, startT, deltaT = 1000, doit = false;
@@ -35,52 +46,43 @@ let ra=0, rb=0, startT, deltaT = 1000, doit = false;
 
 function setup() 
 {
+  var gui = createGui('Colour Picker GUI');
+  information = "Press P to start/pause - Press the Space bar to reset the canvas to use your new colour, and then press P to restart";
+  squareSize = 20;
+  squareSizeRandom = false;
+
+  shapeColour =  '#4F4D92';
+  shapeColourAlpha = 50;
+
+  strokeColour = '#ffffff';
+  strokeColourAlpha = 0;
+
+  backgroundColour = '#ffffff';
+
+
+
+
+
+
+  gui.addGlobals('information', 'squareSize', 'squareSizeRandom', 'shapeColour','shapeColourAlpha', 'strokeColour', 'strokeColourAlpha', 'backgroundColour');
+  
+
+
 
   //----------------------------------------------------------------------------------------VARIABKLE INITIALISE 
 
   createCanvas(windowWidth, windowHeight);
+  
+
+
   stepX = 4;  
   stepY = 20;
   currentY = 0;
   frameRate(0); 
   canSetBackground = true;
-
-  sliderR = createSlider(0, 255, 0, 1);  
-  sliderG = createSlider(0, 255, 0, 1);
-  sliderB = createSlider(0, 255, 0, 1);
-  sliderA = createSlider(0, 255, 128, 1);
-
-  sliderR.position(10, 10);
-  sliderG.position(10, 50);
-  sliderB.position(10, 90);
-  sliderA.position(10, 130);
   
-  sliderR.style('width', '200px');
-  sliderG.style('width', '200px'); 
-  sliderB.style('width', '200px');
-  sliderA.style('width', '200px');
 
-  sliderRText = text("Red Value", sliderR.x, sliderR.y + 32);
-  sliderGText = text("Green Value", sliderG.x, sliderG.y + 32);
-  sliderBText = text("Blue Value", sliderB.x, sliderB.y + 32);
-  sliderAText = text("Alpha Value", sliderA.x, sliderA.y + 32);
-
-  backgroundColourPicker = createColorPicker('#FFFFFF');
-  backgroundColourPicker.position(sliderA.x, sliderA.y + 60);
-  backgroundColourPicker.style('width', '200px');
-
-  
-  pauseButton = createButton("Toggle Pause");
-  pauseButton.mousePressed(pauseSquares);
-  pauseButton.position(backgroundColourPicker.x, backgroundColourPicker.y + 50);
-  pauseButton.style('width', '200px');
   userPaused = true;
-
-  resetButton = createButton("Reset Canvas");
-  resetButton.mousePressed(resetCanvas);  
-  resetButton.position(pauseButton.x, pauseButton.y + 30);
-  resetButton.style('width', '200px');
-
 
 
   prevPos = 0-stepX;
@@ -121,9 +123,6 @@ function setup()
   }
 
 
-   //----------------------------------------------------------------------------------------GUI 
-   //------------------------------------------------------------------------------Colour Change
-
    
   //----------------------------------------------------------------------------------------TIMER 
   startT=millis();
@@ -139,43 +138,49 @@ function setup()
 
 function draw() 
 {
-    //----------------------------------------------------------------------------------------DRAW GUI 
-    // let colourValY = map(distances[index].y, 0, windowHeight, 0.3, 0.8);
-    // let colourValX = map(distances[index].x, 0, windowHeight, 0.01, 0.8);
-
-    // let colourValXY = map(colourValX + colourValY, 0, 2, 0.5, 1);
-    // console.log(colourValXY);
 
     if(canSetBackground)
     {
-      background(backgroundColourPicker.color());
+      var backgroundColourRGB= color(backgroundColour);
+      var r = backgroundColourRGB._getRed();
+      var g = backgroundColourRGB._getGreen();
+      var b = backgroundColourRGB._getBlue();      
+
+      background(r, g, b);
+      
       canSetBackground = false;
     }
 
-    let r = sliderR.value();
-    let g = sliderG.value();
-    let b = sliderB.value();
-    let a = sliderA.value();    
 
-    stroke(0, 0, 0, 255);
-    square(sliderR.x + 220, (sliderR.y + sliderA.y) / 2, 40);
-    stroke(0, 0, 0, 0);
-
-      
-    fill(r, g, b, a);
 
   //----------------------------------------------------------------------------------------DRAW SQUARES
 
-  square(distances[index].x, distances[index].y, random(10, 100));
+
+
+  var strokeColourRGB= color(strokeColour);
+  var r = strokeColourRGB._getRed();
+  var g = strokeColourRGB._getGreen();
+  var b = strokeColourRGB._getBlue();
+  
+  stroke(r, g, b, strokeColourAlpha);
+
+  var shapeColorRGB= color(shapeColour);
+  r = shapeColorRGB._getRed();
+  g = shapeColorRGB._getGreen();
+  b = shapeColorRGB._getBlue();
+
+  fill(r, g, b, shapeColourAlpha);
+
+  if(squareSizeRandom)
+  {
+    square(distances[index].x, distances[index].y, random(10, 100));
+
+  }
+  else
+  square(distances[index].x, distances[index].y, squareSize);
+
   index++;
 
-
- 
-  
-      
-    
-
-  
 
 }
 
@@ -203,4 +208,41 @@ function resetCanvas()
   userPaused = true;
   canSetBackground = true;
   frameRate(0);
+}
+
+function toggleStroke()
+{
+
+}
+
+function keyPressed() 
+{
+
+  if(keyCode === 32) //SPACE
+  {
+    resetCanvas();
+  }
+
+  if (keyCode === 71) // KEY G
+  {
+    if(showGUI) showGUI = false;
+    else showGUI = true;
+  } 
+  else {
+    value = 0;
+  }
+
+  if(keyCode === 80) //KEY P
+  {
+    pauseSquares();
+  }
+
+
+
+ 
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
